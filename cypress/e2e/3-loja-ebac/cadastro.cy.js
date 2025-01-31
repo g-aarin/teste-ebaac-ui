2e825bf8ce92fb0a13d215335bafd86b0eefcae9
Gustavo Araujo <gustavoarju2323@gmail.com>
2025-01-31 11:02:59 -0300

    Uso de Variaveis e Bibliotecas

---
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Cadastro', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it('Deve completar o cadastro com sucesso - Usando variáveis', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('guts2025')
+        cy.get(':nth-child(4) > .button').click()        
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName()) 
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso')
+    });
+
+    it('Deve completar o cadastro com sucesso - Usando variáveis', () => {
+        var email = faker.internet.email()
+        var nome = faker.person.firstName()
+        var sobrenome = faker.person.lastName()
+
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('guts2025')
+        cy.get(':nth-child(4) > .button').click()        
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName()) 
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso')
+    });
+});
