346e05c37e34a0141de05f9965b33b058849a511
Gustavo Araujo <gustavoarju2323@gmail.com>
2025-02-01 10:57:28 -0300

    Finalizando testes ex aula

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+        .contains('Aero Daily Fitness Tee')
+        //.last()   
+        //.eq(1)
+        //.first()
+        .click()
+        cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    });
+});
